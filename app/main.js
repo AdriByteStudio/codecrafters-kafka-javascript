11e9c0d93a21703ba6fd5187fa8e5b821d6e0fc8
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-11 18:06:40 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -15,10 +15,14 @@ const server = net.createServer((connection) => {
       return;
     }
 
+    const requestApiVersion = buffer.readInt16BE(6);
     const correlationId = buffer.readInt32BE(8);
-    const response = Buffer.alloc(8);
+    const errorCode = requestApiVersion >= 0 && requestApiVersion <= 4 ? 0 : 35;
+
+    const response = Buffer.alloc(10);
     response.writeInt32BE(0, 0);
     response.writeInt32BE(correlationId, 4);
+    response.writeInt16BE(errorCode, 8);
 
     connection.end(response);
   });
