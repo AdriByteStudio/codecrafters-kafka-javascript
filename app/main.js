f5f878eb6f5faac7ab44fc1f616a9586268dcadc
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-11 18:36:17 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,35 +6,36 @@ const server = net.createServer((connection) => {
   connection.on("data", (chunk) => {
     buffer = Buffer.concat([buffer, chunk]);
 
-    if (buffer.length < 4) {
-      return;
+    while (buffer.length >= 4) {
+      const messageSize = buffer.readInt32BE(0);
+      if (buffer.length < 4 + messageSize) {
+        break;
+      }
+
+      const request = buffer.subarray(0, 4 + messageSize);
+      buffer = buffer.subarray(4 + messageSize);
+
+      const requestApiVersion = request.readInt16BE(6);
+      const correlationId = request.readInt32BE(8);
+      const errorCode = requestApiVersion >= 0 && requestApiVersion <= 4 ? 0 : 35;
+
+      const responseBody = Buffer.alloc(15);
+      responseBody.writeInt16BE(errorCode, 0);
+      responseBody.writeInt8(2, 2);
+      responseBody.writeInt16BE(18, 3);
+      responseBody.writeInt16BE(0, 5);
+      responseBody.writeInt16BE(4, 7);
+      responseBody.writeInt8(0, 9);
+      responseBody.writeInt32BE(0, 10);
+      responseBody.writeInt8(0, 14);
+
+      const response = Buffer.alloc(4 + 4 + responseBody.length);
+      response.writeInt32BE(4 + responseBody.length, 0);
+      response.writeInt32BE(correlationId, 4);
+      responseBody.copy(response, 8);
+
+      connection.write(response);
     }
-
-    const messageSize = buffer.readInt32BE(0);
-    if (buffer.length < 4 + messageSize) {
-      return;
-    }
-
-    const requestApiVersion = buffer.readInt16BE(6);
-    const correlationId = buffer.readInt32BE(8);
-    const errorCode = requestApiVersion >= 0 && requestApiVersion <= 4 ? 0 : 35;
-
-    const responseBody = Buffer.alloc(15);
-    responseBody.writeInt16BE(errorCode, 0);
-    responseBody.writeInt8(2, 2);
-    responseBody.writeInt16BE(18, 3);
-    responseBody.writeInt16BE(0, 5);
-    responseBody.writeInt16BE(4, 7);
-    responseBody.writeInt8(0, 9);
-    responseBody.writeInt32BE(0, 10);
-    responseBody.writeInt8(0, 14);
-
-    const response = Buffer.alloc(4 + 4 + responseBody.length);
-    response.writeInt32BE(4 + responseBody.length, 0);
-    response.writeInt32BE(correlationId, 4);
-    responseBody.copy(response, 8);
-
-    connection.end(response);
   });
 });
 
