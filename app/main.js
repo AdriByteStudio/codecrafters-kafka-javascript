37caba9517c077f4855464e20a13825cb7d80844
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-11 17:59:58 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 import net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const response = Buffer.alloc(8);
+response.writeInt32BE(0, 0);
+response.writeInt32BE(7, 4);
 
-// TODO: Uncomment the code below to pass the first stage
- const server = net.createServer((connection) => {
-   // Handle connection
- });
+const server = net.createServer((connection) => {
+  connection.end(response);
+});
 
- server.listen(9092, "127.0.0.1");
+server.listen(9092, "127.0.0.1");
